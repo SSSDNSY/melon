3b5dd72a528283a866d084f860d61351597a4f7d
彭智慧 <pengzh666@163.com>
2022-08-03 17:30:30 +0800

    feat:i18n

--- src/main/java/fun/sssdnsy/melon/controller/PagesController.java
+++ src/main/java/fun/sssdnsy/melon/controller/PagesController.java
@@ -1,6 +1,5 @@
 package fun.sssdnsy.melon.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -9,12 +8,16 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @class PagesController
  * @since 2022-07-28
  */
-@Controller
 public class PagesController {
 
     @GetMapping(value = {"/", "/home"})
     public String Home() {
         return "home";
+    }
+
+    @GetMapping(value = {"/error"})
+    public String error() {
+        return "" + (1 / 0);
     }
 
 
