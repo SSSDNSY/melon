84e65aa13483bd1c187ce93c32236ab261272662
彭智慧 <pengzh666@163.com>
2022-08-09 15:43:02 +0800

    【init】 IndexController

---
+++ admin/src/main/java/fun/sssdnsy/admin/controller/IndexController.java
@@ -0,0 +1,15 @@
+package fun.sssdnsy.admin.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @desc  页面路由
+ * @class IndexController
+ * @since 2022-08-09
+ */
+@Controller
+public class IndexController {
+
+
+
+}
