ac6d4339e3b940463c5d18e00bcf32808c9e303e
彭智慧 <pengzh666@163.com>
2022-08-09 15:07:29 +0800

    【init】 Dockerfile + logback.xml

---
+++ admin/src/main/java/fun/sssdnsy/admin/Application.java
@@ -0,0 +1,18 @@
+package fun.sssdnsy.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @desc  App
+ * @class Application
+ * @since 2022-08-09
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
