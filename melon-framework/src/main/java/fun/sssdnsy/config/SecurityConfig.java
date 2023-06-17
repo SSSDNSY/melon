16ddcce1d355525ab15e16b802d4c5621ffdc332
pengzh <736259020@qq.com>
2023-06-18 03:13:30 +0800

    feat: Gitee 授权、登录改造

--- melon-framework/src/main/java/fun/sssdnsy/config/SecurityConfig.java
+++ melon-framework/src/main/java/fun/sssdnsy/config/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // 过滤请求
                 .authorizeRequests()
                 // 对于登录login 注册register 验证码captchaImage Oauth 允许匿名访问
-                .antMatchers("/login", "/register", "**/captchaImage", "/oauth/**").permitAll()
+                .antMatchers("/login", "/register", "**/captchaImage", "**/oauth/**").permitAll()
                 //websocket
                 .antMatchers( "/endpoint/**" ).permitAll()
                 //监控中心
