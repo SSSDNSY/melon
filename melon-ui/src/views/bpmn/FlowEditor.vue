9fe2e99384b88c4a084a8cdbd7cbf95908920a65
彭智慧 <sssdnsy@163.com>
2023-07-05 11:26:32 +0800

    feat: vue bpmn editor 流程编辑 https://github.com/miniclound/vue-bpmn/tree/master

---
+++ melon-ui/src/views/bpmn/FlowEditor.vue
@@ -0,0 +1,16 @@
+<template>
+  <Bpmn/>
+</template>
+
+<script>
+import Bpmn from "@/components/Bpmn";
+
+export default {
+  components: {
+    Bpmn
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
