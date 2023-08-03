2a024148f14d4a4db53ea721e5b8287aafd57a93
彭智慧 <sssdnsy@163.com>
2023-08-03 09:49:40 +0800

    fix: login messageBox fixed

--- melon-ui/src/utils/request.js
+++ melon-ui/src/utils/request.js
@@ -76,7 +76,7 @@ service.interceptors.response.use(res => {
       return res.data
     }
     if (code === 401) {
-      if (!isRelogin.show) {
+      if (!isRelogin.show && location.href.indexOf("login")===-1) {
         isRelogin.show = true;
         MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', { confirmButtonText: '重新登录', cancelButtonText: '取消', type: 'warning' }).then(() => {
           isRelogin.show = false;
